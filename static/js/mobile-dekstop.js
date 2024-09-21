5e9c7c5361bdb6efc2f63826fd84782b67511f0c
Flavien <flavienbriendo@gmail.com>
2024-09-21 17:30:35 +0200

    feat: base

--- static/js/mobile-dekstop.js
+++ static/js/mobile-dekstop.js
@@ -1,24 +1,3 @@
-// "Height" "Width" "Margin Top" "Margin Right" "Margin Bottom" "Margin left"
-
-const info_dekstop = ['80vh', '20%', '10vh', '90%', '10vh', '0%'];
-const info_mobile = ['10vh', '100%', '10vh', '0%', '80vh', '0%'];
-
-const content_dekstop = ['80vh', '80%', '10vh', '0%', '10vh', '20%'];
-const content_mobile = ['70vh', '100%', '20vh', '0%', '10vh', '0%'];
-
-const content_info_hide_dekstop = ['80vh', '100%', '10vh', '0%', '10vh', '0%'];
-const content_info_hide_mobile = ['80vh', '100%', '10vh', '0%', '10vh', '0%'];
-
-var viewportWidth = window.innerWidth;
-var viewportHeight = window.innerHeight;
-
-var is_dekstop = true
-var is_info_display = false
-
-if (viewportHeight > viewportWidth) {
-    is_dekstop = false
-}
-
 const info_CSS = document.querySelector('.info');
 const content_CSS = document.querySelector('.content');
 const header_CSS = document.querySelector('header');
@@ -29,7 +8,7 @@ info.style.display ='none'
 const content = document.getElementById("content");
 const button_menu = document.getElementById("button_display_menu");
 
-function set_display_info()
+export function set_display_info()
 {
     is_info_display = true
     console.log("Bahahaha1");
@@ -42,7 +21,7 @@ function set_display_info()
     content_CSS.style.height = content_mobile[0];
 }
 
-function set_close_info()
+export function set_close_info()
 {
     is_info_display = false
     console.log("Bahahaha");
@@ -55,17 +34,7 @@ function set_close_info()
     content_CSS.style.top = content_info_hide_mobile[2];
 }
 
-button_menu.addEventListener("click", function() {
-    if (info.style.display == 'none') {
-        info.style.display = 'flex';
-        set_display_info()
-        return;
-    }
-    info.style.display ='none';
-    set_close_info()
-});
-
-function set_dekstop() {
+export function set_dekstop() {
     var size = Math.trunc(get_size(viewportWidth, viewportHeight, 3)) + 'vh'
     document.querySelectorAll('.menu').forEach(function(menu) {
         menu.style.fontSize = size;
@@ -100,7 +69,7 @@ function set_dekstop() {
     }
 }
 
-function set_mobile() {
+export function set_mobile() {
     var size = Math.trunc(get_size(viewportWidth, viewportHeight, 3)) + 'vw'
     document.querySelectorAll('.menu').forEach(function(menu) {
         menu.style.fontSize = size;
@@ -134,38 +103,3 @@ function set_mobile() {
         set_close_info()
     }
 }
-
-function get_size(winWidth, winHeight, number) {
-    console.log(is_dekstop)
-    if (is_dekstop) {
-        return Math.trunc(500 * number / winHeight) + 'vh'
-    }
-    return Math.trunc(500 * number / winWidth) + 'vw'
-}
-
-function handleResize() {
-    viewportWidth = window.innerWidth;
-    viewportHeight = window.innerHeight;
-
-    if (is_dekstop && viewportWidth < viewportHeight) {
-        is_dekstop = false;
-        set_mobile();
-        console.log("kjfezhbezk")
-    }
-    else if (!is_dekstop && viewportWidth > viewportHeight) {
-        is_dekstop = true;
-        set_dekstop();
-    }
-    let size = get_size(viewportWidth, viewportHeight, 3)
-    console.log("Mé")
-    //console.log('Window width:', viewportWidth);
-    //console.log('Window height:', viewportHeight);
-    document.querySelectorAll('.menu').forEach(function(menu) {
-        menu.style.fontSize = 10 + (is_dekstop ? 'vh' : 'vw');
-        menu.style.paddingLeft = size;
-        menu.style.paddingRight = size;
-    });
-}
-
-window.addEventListener('resize', handleResize);
-window.onload = handleResize;
