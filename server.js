f06b9e58ab3e2473da5d962ef6930b038768fa95
Flavien <flavienbriendo@gmail.com>
2024-09-25 21:30:28 +0200

    update: server + style

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static('.'));
 
+/*
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'server.cert'))
+};*/
+
 // Home
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/content/file/", (req, res) => {
+app.get("/content/file/*", (req, res) => {
   res.sendFile(path.join(__dirname, 'contents', 'file', req.params[0]));
 });
 
@@ -28,3 +36,7 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+/*https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});*/
